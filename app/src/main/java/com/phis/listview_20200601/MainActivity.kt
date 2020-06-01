7c0956337f1064b0816f9e32464d8ee65800b32e
kswiiz0 <kswiiz0@gmail.com>
2020-06-01 22:07:10 +0900

    getView 내부 수정  - 임시로 이름만 출력되도록 수정

--- app/src/main/java/com/phis/listview_20200601/MainActivity.kt
+++ app/src/main/java/com/phis/listview_20200601/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.phis.listview_20200601
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.phis.listview_20200601.adapters.StudentAdapter
 import com.phis.listview_20200601.datas.Student
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
     val students = ArrayList<Student>()
+    lateinit var studentAdapter: StudentAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +31,14 @@ class MainActivity : BaseActivity() {
         students.add(Student("김형근",1989,true))
         students.add(Student("이용희",1992,true))
         students.add(Student("이지수",1984,false))
+
+
+
+        studentAdapter = StudentAdapter(mContext, R.layout.student_list_view, students)
+        studentListView.adapter = studentAdapter
+
+
+
 
     }
 
--- app/src/main/java/com/phis/listview_20200601/adapters/StudentAdapter.kt
+++ app/src/main/java/com/phis/listview_20200601/adapters/StudentAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.phis.listview_20200601.R
 import com.phis.listview_20200601.datas.Student
 
@@ -24,6 +25,14 @@ class StudentAdapter(context: Context, resId: Int, list: List<Student>) :
         }
 
         val row = tempRow!!
+        val nameAndAgeTxt = row.findViewById<TextView>(R.id.nameAndAgeTxt)
+        val genderTxt = row.findViewById<TextView>(R.id.genderTxt)
+
+
+        val data = mList.get(position)
+        nameAndAgeTxt.text = data.name
+
+
         return row
     }
 
