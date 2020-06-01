facded84ea0a7395337bc6dee856595909dca26b
kswiiz0 <kswiiz0@gmail.com>
2020-06-01 21:48:48 +0900

    목록변수(ArrayList)를 생성하고 데이터 추가

--- app/src/main/java/com/phis/listview_20200601/MainActivity.kt
+++ app/src/main/java/com/phis/listview_20200601/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.phis.listview_20200601
 
 import android.os.Bundle
+import com.phis.listview_20200601.datas.Student
 
 class MainActivity : BaseActivity() {
+
+    val students = ArrayList<Student>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +20,12 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        students.add(Student("조경진",1988,true))
+        students.add(Student("김미영",2010,false))
+        students.add(Student("김성우",1982,true))
+        students.add(Student("김형근",1989,true))
+        students.add(Student("이용희",1992,true))
+        students.add(Student("이지수",1984,false))
 
     }
 
